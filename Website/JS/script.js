b3cf78b856716b59d371f8feef5392a660c43008
JayNg96 <S10208269@connect.np.edu.sg>
2021-02-05 21:21:37 +0800

    Add social panel JS styled with CSS

--- Website/JS/script.js
+++ Website/JS/script.js
@@ -257,3 +257,17 @@ document.addEventListener("DOMContentLoaded", () => {
       ui.cartLogic();
     });
 });
+
+// social panel JS
+const floating_btn = document.querySelector('.floating-btn');
+const close_btn = document.querySelector('.close-btn');
+const social_panel_container = document.querySelector('.social-panel-container');
+
+floating_btn.addEventListener('click', () => {
+    social_panel_container.classList.toggle('visible')
+});
+
+close_btn.addEventListener('click', () => {
+    social_panel_container.classList.remove('visible');
+});
+
